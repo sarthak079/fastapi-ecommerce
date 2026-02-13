8bfaceabf1e8503d72120301bb6a21ed4be43e06
Sarthak Singhal <sarthaksinghal3107@gmail.com>
2026-02-14 01:13:49 +0530

    update to schema

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, HTTPException, Query, Path
 from pydantic import BaseModel, Field
 from typing import List, Optional, Annotated
-from app.service.products import get_all_products
 from app.schemas.products import Products
+from uuid import uuid4,UUID
+from datetime import datetime
+from app.service.products import add_product,get_all_products,delete_products
 
 app=FastAPI()
 
@@ -15,5 +17,21 @@ def read_root():
 
 @app.post("/products",status_code=201)
 def create_product(product: Products):
+    product_dict=product.model_dump(mode="json")
+    product_dict["id"]=str(uuid4())
+    product_dict["created_at"]=datetime.utcnow().isoformat()+"Z"
+    try:
+        add_product(product_dict)
+    except ValueError as e:
+        raise HTTPException(status_code=400,detail=str(e))
     return product.model_dump(mode="json")
+
+@app.delete("/products/{products_id}")
+def delete_product(products_id: UUID=Path(...,description="Product UUID")):
+    try:
+        res=delete_products(str(products_id))
+        return res
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=str(e))
+
 
